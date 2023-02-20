464e6ff3c40b187f66caf6233f962c2b89aa2b07
karenbartaud <bartaud.karen@gmail.com>
2023-02-20 20:35:07 +0100

    card component into filtered

---
+++ src/components/app/FilteredActivities/index.js
@@ -0,0 +1,23 @@
+import './style.scss';
+import Card from '../FilteredActivities/card/card';
+
+function Filtered() {
+
+
+    return (
+        <div className="cards">
+
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+</div>
+    );
+  }
+
+  export default Filtered;
