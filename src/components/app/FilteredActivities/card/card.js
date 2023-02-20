5f65e1e0611968588a1bcacdf2594de19b9505c7
karenbartaud <bartaud.karen@gmail.com>
2023-02-20 20:05:06 +0100

    ebauche card

--- src/components/app/FilteredActivities/card/card.js
+++ src/components/app/FilteredActivities/card/card.js
@@ -1,14 +1,36 @@
 import './style.scss';
 import Running_icon from '../../../../assets/running.png';
+import Mountain from '../../../../assets/mountain.jpg';
 
 function Card() {
     
   
     return (
 
-    <div className="card">
-        <h1>TITLE</h1>
-    </div>    
+        
+        <div class="ui card">
+        <div class="content">
+          <div class="right floated meta">14h</div>
+          <img class="ui avatar image" src={`${Mountain}`} /> Elliot
+        </div>
+        <div class="image">
+          <img />
+        </div>
+        <div class="content">
+          <span class="right floated">
+            <i class="heart outline like icon"></i>
+            17 likes
+          </span>
+          <i class="comment icon"></i>
+          3 comments
+        </div>
+        <div class="extra content">
+          <div class="ui large transparent left icon input">
+            <i class="heart outline icon"></i>
+            <input type="text" placeholder="Add Comment..." />
+          </div>
+        </div>
+      </div>
 
 
     );
