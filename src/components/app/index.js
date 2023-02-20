7e49de6288397ed9e1e4fcf83973ae56c6aa392a
Ramazan <ramazancinar0858@gmail.com>
2023-02-20 13:27:41 +0100

    structure : components

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import './style.scss';
+import 'semantic-ui-css/semantic.min.css'
+
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+<Button className="ui button">Click Here</Button>
+    </div>
+  );
+}
+
+export default App;
