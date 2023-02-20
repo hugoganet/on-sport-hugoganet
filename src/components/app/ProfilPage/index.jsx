ce01f4367e02461e3fbd3c563dfc62e942f31cb5
Hugo Ganet <ganethugo@gmail.com>
2023-02-20 16:19:05 +0100

    Config : add esLint

---
+++ src/components/app/ProfilPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+function ProfilPage() {
+  return (
+    <div className="ProfilPage">
+      <h1>Hello World !</h1>
+    </div>
+  );
+}
+
+export default ProfilPage;
--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,14 +1,16 @@
+import React from 'react';
+
 import './style.scss';
 import 'semantic-ui-css/semantic.min.css';
 
-
 import Home from './Home';
+import ProfilPage from './ProfilPage';
 
 function App() {
   return (
     <div className="App">
       <Home />
-
+      <ProfilPage />
     </div>
   );
 }
