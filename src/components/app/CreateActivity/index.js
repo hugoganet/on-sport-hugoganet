690fb27a6883002b271a1d8edd7074387d23aa0e
Ramazan <ramazancinar0858@gmail.com>
2023-02-20 23:21:27 +0100

    CreateActivity component : form completed

--- src/components/app/CreateActivity/index.js
+++ src/components/app/CreateActivity/index.js
@@ -3,18 +3,38 @@ import Footer from "../Footer";
 import Header from "../Header";
 
 function CreateActivity() {
-    return (
+    const options = [
+        { key: 'ft', text: 'Footing', value:'footing'},
+        { key: 'vl', text: 'Vélo', value:'velo'},
+        { key: 'ft', text: 'Escalade', value:'escalade'},
+    ];
+
+        return (
         <>
             <Header />
             <h1>Ajouter une activité</h1>
-            <Form className="ui form">
-                <div className="field">
-                    <div className="two fields">
-                        <div className="field">
-                            <input type="text" name="title" placeholder="Titre de l'activité" />
-                        </div>
-                    </div>
-                </div>
+            <Form>
+                <Form.Input fluid label="Entrer le titre de l'activité" placeholder="Titre de l'activité" />
+                <Form.Input fluid label="Entrer la localisation" placeholder="Localisation" />
+                <Form.Group inline>
+                    <Form.Select
+                        fluid
+                        label="Sport"
+                        options={options}
+                    />
+                    <label>Cette activité peut-elle se faire en famille ? </label>
+                    <Form.Radio
+                    label="Oui"
+                    value="oui"
+                    // checked={value === 'oui'}
+                    />
+                    <Form.Radio
+                    label="Non"
+                    value="non"
+                    />
+                </Form.Group>
+                <Form.TextArea label="Description de l'activité" placeholder="Ajouter une description de l'activité" />
+                <Form.Button>Valider</Form.Button>
             </Form>
             <Footer />
         </>
