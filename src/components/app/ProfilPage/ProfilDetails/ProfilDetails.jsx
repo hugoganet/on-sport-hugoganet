2365d5eeebfc1cbef499d63f0418db3f37d058ba
Hugo Ganet <ganethugo@gmail.com>
2023-02-21 19:27:57 +0100

    style(ProfilDetails) : adding some div and section for the css and begining of media queries

--- src/components/app/ProfilPage/ProfilDetails/ProfilDetails.jsx
+++ src/components/app/ProfilPage/ProfilDetails/ProfilDetails.jsx
@@ -6,19 +6,25 @@ import './style.scss';
 function ProfilDetails() {
   return (
     <div className="ProfilDetails">
-      <p className="ProfilDetails__location">Ambert - Auvergne-Rhône-Alpes</p>
-      <p className="ProfilDetails__age">42 ans</p>
-      <h2 className="ProfilDetails__bio--title">Bio</h2>
-      <p className="ProfilDetails__bio--description">
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit...
-      </p>
-      <h2 className="ProfilDetails__practiceSports--title">Sports pratiqués</h2>
-      <div className="ProfilDetails__practiceSports--img--div">
-        <img className="ProfilDetails__practiceSports--img" src={logo} alt="icon" />
-        <img className="ProfilDetails__practiceSports--img" src={logo} alt="icon" />
-        <img className="ProfilDetails__practiceSports--img" src={logo} alt="icon" />
-      </div>
+      <p className="ProfilDetails__location">📍Ambert - Auvergne-Rhône-Alpes</p>
+      <p className="ProfilDetails__age">⏳42 ans</p>
+      <section className="ProfilDetails__bioAndPracticeSports">
+        <div className="ProfilDetails__bio--div">
+          <h2 className="ProfilDetails__bio--title">Bio</h2>
+          <p className="ProfilDetails__bio--description">
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit...
+          </p>
+        </div>
+        <div className="ProfilDetails__practiceSports__div">
+          <h2 className="ProfilDetails__practiceSports__div--title">Sports pratiqués</h2>
+          <div className="ProfilDetails__practiceSports__div--div">
+            <img className="ProfilDetails__practiceSports__div--img" src={logo} alt="icon" />
+            <img className="ProfilDetails__practiceSports__div--img" src={logo} alt="icon" />
+            <img className="ProfilDetails__practiceSports__div--img" src={logo} alt="icon" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
