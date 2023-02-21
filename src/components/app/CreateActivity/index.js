51382ef3daaf6103de3395496c251e9ed43cedd5
Ramazan <ramazancinar0858@gmail.com>
2023-02-21 11:42:19 +0100

    CreateActivity component : add image button & default image

--- src/components/app/CreateActivity/index.js
+++ src/components/app/CreateActivity/index.js
@@ -1,6 +1,10 @@
+import { Button } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 import { Form } from "semantic-ui-react";
 import Footer from "../Footer";
 import Header from "../Header";
+
+import './style.scss';
 
 function CreateActivity() {
     const options = [
@@ -12,10 +16,10 @@ function CreateActivity() {
         return (
         <>
             <Header />
-            <h1>Ajouter une activité</h1>
             <Form>
-                <Form.Input fluid label="Entrer le titre de l'activité" placeholder="Titre de l'activité" />
-                <Form.Input fluid label="Entrer la localisation" placeholder="Localisation" />
+            <h1 className="">Ajouter une activité</h1>
+                <Form.Input  width={12} fluid label="Entrer le titre de l'activité" placeholder="Titre de l'activité" />
+                <Form.Input width={12} fluid label="Entrer la localisation" placeholder="Localisation" />
                 <Form.Group inline>
                     <Form.Select
                         fluid
@@ -34,9 +38,18 @@ function CreateActivity() {
                     value="non"
                     />
                 </Form.Group>
-                <Form.TextArea label="Description de l'activité" placeholder="Ajouter une description de l'activité" />
+                <Form.TextArea width={12} label="Description de l'activité" placeholder="Ajouter une description de l'activité" />
+                    <Button className="ui button">
+                        <label>Ajouter une image...</label>
+                    </Button>                    
+                <Image.Group size="medium">
+                    <Image src="/default-image.png" />
+                    <Image src="/default-image.png" />
+                    <Image src="/default-image.png" />
+                </Image.Group>
                 <Form.Button>Valider</Form.Button>
             </Form>
+
             <Footer />
         </>
     );
