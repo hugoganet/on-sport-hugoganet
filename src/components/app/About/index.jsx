740976370c3b55ed977a92d492068297aa7d910a
Hugo Ganet <ganethugo@gmail.com>
2023-02-22 08:10:38 +0100

    feat(About) : Create component structure and call it in App/index.js to visualise it

---
+++ src/components/app/About/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.scss';
+
+function About() {
+  return (
+    <div className="About">About</div>
+    // présentation du projet
+    // présentation de l'équipe
+    // carte des membres de l'équipe
+      // photo
+      // nom prénom
+      // petite description
+  );
+}
+
+export default About;
--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import './style.scss';
 import 'semantic-ui-css/semantic.min.css';
 
-import ProfilPage from './ProfilPage';
+// import ProfilPage from './ProfilPage';
 import Footer from './Footer';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
-      <ProfilPage />
+      <About />
+      {/* <ProfilPage /> */}
       <Footer />
     </div>
   );
