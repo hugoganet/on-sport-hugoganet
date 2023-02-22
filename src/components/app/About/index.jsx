10d017a5d7bb5ebc79318fc3b221d8fabe650632
Hugo Ganet <ganethugo@gmail.com>
2023-02-22 12:50:06 +0100

    style(About) : Create styles for responsive design on mobile table and desktop

--- src/components/app/About/index.jsx
+++ src/components/app/About/index.jsx
@@ -6,39 +6,29 @@ import './style.scss';
 
 function About() {
   return (
-    <>
-      <div className="About">
-        <h2 className="About__projectPresentation__title">Présentation du projet</h2>
-        <p className="About__projectPresentation">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
-        </p>
-        <h2 className="About__teamMembers__title">Présentation de l&apos;équipe</h2>
-        <p className="About__teamPresentation">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
-        </p>
-
-        <section className="About__teamMembers">
-          <TeamMemberCard />
-          <TeamMemberCard />
-          <TeamMemberCard />
-          <TeamMemberCard />
-          <TeamMemberCard />
-        </section>
-      </div>
-      {/* // présentation du projet
-      // présentation de l'équipe
-      // carte des membres de l'équipe
-      // photo
-      // nom prénom
-      // petite description */}
-
-    </>
+    <div className="About">
+      <h2 className="About__projectPresentation__title">Présentation du projet</h2>
+      <p className="About__projectPresentation">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it
+        to make a type specimen book.
+      </p>
+      <h2 className="About__teamMembers__title">Présentation de l&apos;équipe</h2>
+      <p className="About__teamPresentation">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it
+        to make a type specimen book.
+      </p>
+      <section className="About__teamMembers">
+        <TeamMemberCard />
+        <TeamMemberCard />
+        <TeamMemberCard />
+        <TeamMemberCard />
+        <TeamMemberCard />
+      </section>
+    </div>
   );
 }
 
