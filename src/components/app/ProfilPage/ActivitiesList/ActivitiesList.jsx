5015e9c2dbfa7112f002925291171c221ade5944
Hugo Ganet <ganethugo@gmail.com>
2023-02-22 07:05:06 +0100

    style(ProfilDetails - ActivitiesList) : finishing media queries for ProfilDetails and ActivitiesLIst

--- src/components/app/ProfilPage/ActivitiesList/ActivitiesList.jsx
+++ src/components/app/ProfilPage/ActivitiesList/ActivitiesList.jsx
@@ -7,10 +7,12 @@ import './style.scss';
 function ActivitiesList() {
   return (
     <div className="ActivitiesList">
-      <h2 className="ActivitiesList__title">Liste des activités créées</h2>
-      <button type="button" className="ActivitiesList__button">
-        🖊 créer une nouvelle activité
-      </button>
+      <div className="ActivitiesList__div--titleAndButton">
+        <h2 className="ActivitiesList__title">Liste des activités créées</h2>
+        <button type="button" className="ActivitiesList__button">
+          🖊 créer une nouvelle activité
+        </button>
+      </div>
       {/* <Card /> */}
     </div>
   );
