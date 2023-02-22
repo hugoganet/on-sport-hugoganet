64006de99387b7811892a23b2560d8a06a08f7db
Ramazan <ramazancinar0858@gmail.com>
2023-02-22 14:21:23 +0100

    DetailledActivity component : add comments

--- src/components/app/DetailledActivity/index.js
+++ src/components/app/DetailledActivity/index.js
@@ -1,9 +1,10 @@
 import Footer from "../Footer";
 import Header from "../Header";
+import Carousel from "./carousel";
+import Comments from "./comment";
 
 import { Image, Item, Header as HeaderUi } from "semantic-ui-react";
 import Filtered from "../FilteredActivities";
-import Carousel from "./carousel";
 
 import './style.scss';
 import { Rating } from "semantic-ui-react";
@@ -12,24 +13,29 @@ function DetailledActivity() {
     return (
         <>
             <Header />
-            <Carousel />
-            <div className="avatar">
-                <Image src="/default-image.png" avatar />
-                <span>Username</span>
-            </div>
-            <div className="activity__title">
-                <HeaderUi as="h1" textAlign="center">Titre de l'activité</HeaderUi>
-            </div>
-            <div className="rating" >
-                <Rating icon="star" defaultRating={3} maxRating={5} disabled size="massive" />
-            </div>
-            <div className="activity__description">
-                <p>Description de l'activité</p>
+            <div className="activity__content">
+                <Carousel />
+                <div className="activity__author">
+                    <Image src="/default-image.png" avatar />
+                    <span>Username</span>
+                </div>
+                <div className="activity__title">
+                    <HeaderUi as="h1" textAlign="center">Titre de l'activité</HeaderUi>
+                </div>
+                <div className="rating" >
+                    <Rating icon="star" defaultRating={3} maxRating={5} disabled size="massive" />
+                </div>
+                <div className="activity__description">
+                    <p>Description de l'activité</p>
+                </div>
+                <Comments />
             </div>
 
-            <div className="filteredActivities">
-                <Filtered />
+
+            <div className="filteredActivities__title">
+                <h1>Autres activités qui pourraient vous intéresser</h1>
             </div>
+                <Filtered />
             <Footer />
         </>
     );
