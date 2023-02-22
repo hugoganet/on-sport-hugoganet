25336306ff2caff728839a6a8166cfd517ed144d
karenbartaud <bartaud.karen@gmail.com>
2023-02-22 09:04:06 +0100

    affichage d'une modale

--- src/components/app/Header/index.js
+++ src/components/app/Header/index.js
@@ -1,21 +1,52 @@
 import Logo from '../../../assets/OnSport_logo.png';
 import './style.scss';
+
+import React from 'react';
+import ModaleLogin from "../../app/modales/modale_login";
+
+import { Button, Header as H, Image, Modal, Form } from 'semantic-ui-react'
 function Header() {
     
-  
-    return (
+return (
         <div className="header">
          
          <div className="ui small image">
             <img src={Logo} alt="logo" />
         </div> 
-        <button class="ui primary button">
+        <button class="ui primary button" >
   SignIn
 </button>
-<button className="ui primary button">
+<button className="ui primary button" onClick={() => {
+           return(
+<ModaleLogin />
+
+  )}} >
  LogIn
 </button>
+
+<>
+      <Modal as={Form} trigger={<Button>Basic Modal</Button>} size="small">
+        <H content="Form Modal" />
+        <Modal.Content>
+          <Form.Input fluid name="title" label="Title" placeholder="Title" />
+          <Form.Input fluid name="author" label="Author" placeholder="Author" />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic color="red">
+            No
+          </Button>
+          <Button type="submit" color="green">
+            Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
+   </>
+
+
 </div>
+
+
+
     );
   }
   
--- src/components/app/Home/index.js
+++ src/components/app/Home/index.js
@@ -2,11 +2,18 @@ import './style.scss';
 import Header from '../../app/Header';
 import Footer from '../../app/Footer';
 import Filtered from '../../app/FilteredActivities';
+import bg from '../../../assets/home_bg.jpg';
+import 'animate.css';
 
 function Home() {
   return (
     <div className="Home">
       <Header />
+      <div className="bg">
+      <img src={bg} alt="background" />
+      <p className="bg-title1 animate__animated animate__backInRight" >Tu es sportif et tu veux continuer à t'entrainer quand tu pars en vacances ?</p>
+      <p className="bg-title2">Tu es veux partager tes expériences sportives à la communauté de OnSporters ?</p>
+      </div>
       <Filtered />
       <Footer />
 
