4e9b848348404446d0dd3fee4d06ee0637918080
Hugo Ganet <ganethugo@gmail.com>
2023-02-24 09:11:01 +0100

    feat : WIP bouton et modals

--- src/components/app/Header/index.jsx
+++ src/components/app/Header/index.jsx
@@ -1,37 +1,58 @@
-import React from 'react';
-import { Button, Icon } from 'semantic-ui-react';
-import Logo from '../../../assets/OnSport_logo.png';
-import './style.scss';
-
-import ModaleSignin from '../modales/modale_signin';
-import ModaleLogin from '../modales/modale_login';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import Modal from './Modal';
 
 function Header() {
-  const [isLogged, setLogin] = React.useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalToggle = () => {
+    setShowModal(!showModal);
+  };
 
   return (
-    <div className="header-header">
-      <div className="ui small image">
-        <img src={Logo} alt="logo" />
-      </div>
-
-      <Button.Group>
-        <Button color="orange" animated="vertical">
-          <Button.Content hidden color="orange">Accueil</Button.Content>
-          <Button.Content visible>
-            <Icon name="home" />
-          </Button.Content>
-        </Button>
-        {isLogged
-          ? <Button onClick={() => alert('IRA SUR LA PAGE PROFIL')}>Mon profil</Button>
-          : <ModaleSignin isLogged={isLogged} setLogin={setLogin} /> }
-        {isLogged
-          ? <Button>Se déconnecter</Button>
-          : <ModaleLogin isLogged={isLogged} setLogin={setLogin} /> }
-      </Button.Group>
+    <div>
+      <button type="button" onClick={handleModalToggle}>Se connecter</button>
+      {showModal && (
+        <Modal onClose={handleModalToggle}>
+          {/* contenu de la modal */}
+        </Modal>
+      )}
     </div>
-
   );
 }
 
 export default Header;
+
+// import React from 'react';
+
+// import Modal from './Modal';
+
+// import Logo from '../../../assets/OnSport_logo.png';
+// import './style.scss';
+
+// function Header() {
+
+//   return (
+//     <div className="header">
+//       <div>
+//         <img className="header__img" src={Logo} alt="logo" />
+//       </div>
+//       <button className="header__button header__button--home" type="button">
+//         Accueil
+//         {/* todo: voir pour ajouter une icone */}
+//       </button>
+
+//       {isLogged
+//       // eslint-disable-next-line no-alert
+//         ? <button type="button" className="header__button header__button--profil" onClick={() => alert('IRA SUR LA PAGE PROFIL')}>Mon profil</button>
+//         // : <ModaleSignin isLogged={isLogged} setLogin={setLogin} /> }
+//         : <button type="button" className="header__button header__button--signIn" onClick={console.log('OUVRIR LA MODAL')}>Connexion</button>}
+//       {isLogged
+//         ? <button type="button" className="header__button header__button--signOut">Se déconnecter</button>
+//         // : <ModaleLogin isLogged={isLogged} setLogin={setLogin} /> }
+//         : <button type="button" className="header__button header__button--signIn" onClick={console.log('OUVRIR LA MODAL')}>Créer un compte</button>}
+//     </div>
+//   );
+// }
+
+// export default Header;
