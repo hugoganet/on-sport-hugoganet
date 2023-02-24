00a14b3b915b06ed7bede947ecf792137608f0a6
Hugo Ganet <ganethugo@gmail.com>
2023-02-24 09:25:53 +0100

    fix : addind new input for card and fix esLint errors

--- src/components/app/FilteredActivities/card/card.jsx
+++ src/components/app/FilteredActivities/card/card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 import './style.scss';
@@ -10,27 +11,26 @@ function Card({
 }) {
   // eslint-disable-next-line no-console
   console.log(logo);
-
   return (
-
     <div className="card">
       <img className="card-bg" src={`${bg}`} alt="" />
       <div className="card-bk" style={{ background: `${color}88` }} />
       <div className="card-logo">
         <img src={`${logo_color}`} alt="" />
       </div>
+      <div className="family_tag1" style={{ color: `${color}` }}>SORTIE</div>
+      <div className="family_tag2" style={{ color: `${color}` }}>FAMILLE</div>
       <div className="card-description">
-        <p>Randonnée soleil levant en montagne</p>
+        <p>Randonnée soleil levant en montagne, avec en prime des chèvres qui me suivent</p>
       </div>
       <div className="card-location">
-        <p>Alpes d&apos;Huez</p>
+        <p>Département</p>
+        <p>Ville</p>
       </div>
-      <div className="card-btn">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">En savoir +</a>
-      </div>
+      <button type="button" className="card-btn" style={{ color: `${color}` }}>
+        En savoir +
+      </button>
     </div>
-
   );
 }
 
