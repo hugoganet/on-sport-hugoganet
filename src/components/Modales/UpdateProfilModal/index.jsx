fcd64992f111b54c6172225d3bd03d8474f32e56
Hugo Ganet <ganethugo@gmail.com>
2023-02-27 18:06:24 +0100

    clean (UpdateProfilModal) : remove a bunch of commented lines

--- src/components/Modales/UpdateProfilModal/index.jsx
+++ src/components/Modales/UpdateProfilModal/index.jsx
@@ -6,7 +6,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-// import axios from 'axios';
+import axios from 'axios';
 
 import './style.scss';
 
@@ -19,50 +19,40 @@ function UpdateProfilModal(props) {
   const [department, setDepartment] = useState('');
   const [sports, setSports] = useState('');
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    onClose();
 
-  // ? Est-ce qu'on a la possibilité de modifier les informations de connextion ?
-  // const [firstName, setFirstName] = useState('');
-  // const [lastName, setLastName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [confirmPassword, setConfirmPassword] = useState('');
-  // const modalRef = useRef(null);
+    const bio = event.target[0].value;
+    const age = event.target[1].value;
+    const city = event.target[2].value;
+    const department = event.target[3].value;
+    const sports = event.target[4].value;
 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   onClose();
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    };
 
-  //   const firstname = event.target[0].value;
-  //   const lastname = event.target[1].value;
-  //   const email = event.target[2].value;
-  //   const login = event.target[3].value;
-  //   const password = event.target[4].value;
+    const response = await axios({
+      method: 'post',
+      url: 'http://ronaldfk-server.eddi.cloud:8080/api/auth/signup',
+      headers: {
+        headers,
+      },
+      data: {
+        bio,
+        age,
+        city,
+        department,
+        sports,
+      },
 
-  //   const headers = {
-  //     'Content-Type': 'application/json',
-  //     'Access-Control-Allow-Origin': '*',
-  //   };
+    });
 
-  //   const response = await axios({
-  //     method: 'post',
-  //     url: 'http://ronaldfk-server.eddi.cloud:8080/api/auth/signup',
-  //     headers: {
-  //       headers,
-  //     },
-  //     data: {
-  //       firstname,
-  //       lastname,
-  //       email,
-  //       login,
-  //       password,
-  //     },
-
-  //   });
-
-  //   // eslint-disable-next-line no-console
-  //   console.log(response.data);
-  // };
+    // eslint-disable-next-line no-console
+    console.log(response.data);
+  };
 
   const handleOutsideClick = (event) => {
     if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -83,59 +73,7 @@ function UpdateProfilModal(props) {
         <h2 className="UpdateProfilModal__title">
           Modifier les informations de mon profil
         </h2>
-        {/* // ? Est-ce qu'on a la possibilité de modifier les informations de connextion ? */}
-        {/* <form className="UpdateProfilModal__form" onSubmit={console.log('SEND THE DATA TO THE BDD')}>
-          <label htmlFor="firstName">Prénom:</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-          />
-          <label htmlFor="lastName">Nom:</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-          />
-          <label htmlFor="email">Adresse email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <label htmlFor="username">Nom d&apos;utilisateur:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-          <label htmlFor="password">Mot de passe:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <label htmlFor="confirm-password">Confirmer le mot de passe:</label>
-          <input
-            type="password"
-            id="confirm-password"
-            name="confirm-password"
-            value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)}
-          />
-          <button type="submit">Créer un compte</button>
-        </form> */}
-        <form className="UpdateProfilModal__form" onSubmit={console.log('SEND THE DATA TO THE BDD')}>
+        <form className="UpdateProfilModal__form" onSubmit={handleSubmit}>
           <label htmlFor="bio">Bio:</label>
           <textarea
             type="text"
@@ -184,7 +122,7 @@ function UpdateProfilModal(props) {
             <option value="volleyball">Volleyball</option>
             <option value="handball">Handball</option>
           </select>
-          <button type="submit">enregistrer les modifications</button>
+          <button type="submit">Enregistrer les modifications</button>
         </form>
       </div>
     </div>
