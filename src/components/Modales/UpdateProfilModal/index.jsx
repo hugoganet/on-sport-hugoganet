eed1b7dea96e297e9c118477d5f8339202d0c6ef
Hugo Ganet <ganethugo@gmail.com>
2023-02-28 12:03:40 +0100

    feat (UpdateProfilModal) : display a list to select practiced sports from data/sports.js

--- src/components/Modales/UpdateProfilModal/index.jsx
+++ src/components/Modales/UpdateProfilModal/index.jsx
@@ -8,6 +8,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+import sportsList from '../../../datas/sports'; // Tableau d'objets importé depuis le fichier
+
 import './style.scss';
 
 function UpdateProfilModal(props) {
@@ -15,9 +17,16 @@ function UpdateProfilModal(props) {
   const { onClose } = props;
   const [bio, setBio] = useState('');
   const [age, setAge] = useState('');
-  const [city, setCity] = useState('');
-  const [department, setDepartment] = useState('');
-  const [sports, setSports] = useState('');
+  const [locationPostCode, setLocationPostCode] = useState('');
+  const [locationName, setLocationName] = useState('');
+  const [locationDepartment, setLocationDepartment] = useState('');
+  const [sports, setSelectedSports] = useState([]);
+
+  function handleChange(event) {
+    const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value);
+    setSelectedSports(selectedOptions);
+    console.log(selectedOptions);
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -25,8 +34,9 @@ function UpdateProfilModal(props) {
 
     const bio = event.target[0].value;
     const age = event.target[1].value;
-    const city = event.target[2].value;
-    const department = event.target[3].value;
+    const locationPostCode = event.target[2].value;
+    const locationDepartment = event.target[3].value;
+    const locationName = event.target[3].value;
     const sports = event.target[4].value;
 
     const headers = {
@@ -35,16 +45,17 @@ function UpdateProfilModal(props) {
     };
 
     const response = await axios({
-      method: 'post',
-      url: 'http://ronaldfk-server.eddi.cloud:8080/api/auth/signup',
+      method: 'PATCH',
+      url: 'http://ronaldfk-server.eddi.cloud:8080/api/user/profil/:id',
       headers: {
         headers,
       },
       data: {
         bio,
         age,
-        city,
-        department,
+        locationPostCode,
+        locationName,
+        locationDepartment,
         sports,
       },
 
@@ -74,53 +85,65 @@ function UpdateProfilModal(props) {
           Modifier les informations de mon profil
         </h2>
         <form className="UpdateProfilModal__form" onSubmit={handleSubmit}>
-          <label htmlFor="bio">Bio:</label>
+          <label className="UpdateProfilModal__form--label" htmlFor="bio">Bio:</label>
           <textarea
+            className="UpdateProfilModal__form--input"
             type="text"
             id="bio"
             name="bio"
             value={bio}
             onChange={(event) => setBio(event.target.value)}
           />
-          <label htmlFor="age">Age:</label>
+          <label className="UpdateProfilModal__form--label" htmlFor="age">Age:</label>
           <input
+            className="UpdateProfilModal__form--input"
             type="number"
             id="age"
             name="age"
             value={age}
             onChange={(event) => setAge(event.target.value)}
           />
-          <label htmlFor="city">Ville</label>
+          <label className="UpdateProfilModal__form--label" htmlFor="locationPostCode">Code postale</label>
           <input
-            type="string"
-            id="city"
-            name="city"
-            value={city}
-            onChange={(event) => setCity(event.target.value)}
+            className="UpdateProfilModal__form--input"
+            type="number"
+            id="locationPostCode"
+            name="locationPostCode"
+            value={locationPostCode}
+            onChange={(event) => setLocationPostCode(event.target.value)}
           />
-          <label htmlFor="department">Departement</label>
+          <label className="UpdateProfilModal__form--label" htmlFor="locationName">Ville</label>
           <input
+            className="UpdateProfilModal__form--input"
             type="string"
-            id="department"
-            name="department"
-            value={department}
-            onChange={(event) => setDepartment(event.target.value)}
+            id="locationName"
+            name="locationName"
+            value={locationName}
+            onChange={(event) => setLocationName(event.target.value)}
           />
-          <label htmlFor="sports">Sports pratiqués</label>
+          <label className="UpdateProfilModal__form--label" htmlFor="locationDepartment">Departement</label>
+          <input
+            className="UpdateProfilModal__form--input"
+            type="string"
+            id="locationDepartment"
+            name="locationDepartment"
+            value={locationDepartment}
+            onChange={(event) => setLocationDepartment(event.target.value)}
+          />
+          <label className="UpdateProfilModal__form--label" htmlFor="sports">Sports pratiqués</label>
           <select
+            className="UpdateProfilModal__form--input"
             aria-label="sports"
             type="string"
             id="sports"
             name="sports"
+            multiple
             value={sports}
-            onChange={(event) => setSports(event.target.value)}
+            onChange={handleChange}
           >
-            <option value="football">Football</option>
-            <option value="basketball">Basketball</option>
-            <option value="tennis">Tennis</option>
-            <option value="rugby">Rugby</option>
-            <option value="volleyball">Volleyball</option>
-            <option value="handball">Handball</option>
+            {sportsList.map((sport) => (
+              <option key={sport.id} value={sport.value}>{sport.text}</option>
+            ))}
           </select>
           <button type="submit">Enregistrer les modifications</button>
         </form>
@@ -133,4 +156,5 @@ UpdateProfilModal.propTypes = {
   onClose: PropTypes.func.isRequired,
 };
 
+// export SportsDropdown;
 export default UpdateProfilModal;
