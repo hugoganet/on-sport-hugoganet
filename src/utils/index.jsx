1cda084ab0eabef7b63caf895f714ffdae1d3478
karenbartaud <bartaud.karen@gmail.com>
2023-03-01 12:23:30 +0100

    filtered activities by sports updated

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,12 +1,15 @@
-function filterActivities(array, filters) {
+function FilterActivities(array, filters) {
+  const getValue = (value) => (typeof value === 'string' ? value.toUpperCase() : value);
+
   const filterKeys = Object.keys(filters);
-  return array.filter((item) => (
+  return array.filter((item) =>
     // validates all filter criteria
+    // eslint-disable-next-line implicit-arrow-linebreak
     filterKeys.every((key) => {
       // ignores an empty filter
       if (!filters[key].length) return true;
-      return filters[key].find((filter) => filter === item[key]);
-    })));
+      return filters[key].find((filter) => getValue(filter) === getValue(item[key]));
+    }));
 }
 
-export default filterActivities;
+export default FilterActivities;
