c3b48fdf98d43464df3f4ef6084d3d1c9ef0c139
Ramazan <ramazancinar0858@gmail.com>
2023-03-08 14:55:03 +0100

    Contact page : add new form

--- src/components/app/Contact/index.jsx
+++ src/components/app/Contact/index.jsx
@@ -1,17 +1,102 @@
-import React from 'react';
+/* eslint-disable import/no-unresolved */
+// import React from 'react';
 
 import './style.scss';
 
+import React, { useState } from 'react';
+import {
+  Form, Button, Message, Grid,
+} from 'semantic-ui-react';
+import Header from '../Header';
+import Footer from '../Footer';
+
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [formError, setFormError] = useState(false);
+  // const [emailSent, setEmailSent] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !message) {
+      setFormError(true);
+      return;
+    }
+
+    // Compose l'e-mail avec les informations du formulaire
+    const mailToLink = `mailto:contact.onsport@gmail.com?subject=Message de ${name}&body=${message}`;
+
+    // Ouvre la fenêtre de composition de l'e-mail
+    window.location.href = mailToLink;
+
+    setName('');
+    setEmail('');
+    setMessage('');
+    // setEmailSent(true);
+  };
+
   return (
-    <form className="Contact">
-      <h2 className="Contact__title">Nous contacter</h2>
-      <input className="Contact__input Contact__input--lastName" type="text" placeholder="Nom" />
-      <input className="Contact__input Contact__input--firstName" type="text" placeholder="Prénom" />
-      <input className="Contact__input Contact__input--email" type="email" placeholder="Email" />
-      <input className="Contact__input Contact__input--message" type="text" placeholder="Votre message" />
-      <button type="button" className="Contact__button">Envoyer</button>
-    </form>
+    <>
+      <Header />
+      <h1 className="Contact__title">Nous contacter</h1>
+      <Grid className="Contact__div" centered>
+        <Form className="Contact__form" onSubmit={handleSubmit} error={formError}>
+          <Form.Input
+            label="Nom et prénom"
+            placeholder="Entrez votre nom et votre prénom"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <Form.Input
+            label="Email"
+            placeholder="Entrez votre email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Form.TextArea
+            label="Message"
+            placeholder="Entrez votre message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+          <Message
+            error
+            header="Erreur"
+            content="Veuillez remplir tous les champs."
+          />
+          <Button type="submit">Envoyer</Button>
+          <Button
+            onClick={() => {
+              // Compose l'e-mail avec les informations du formulaire
+              const mailToLink = `mailto:contact.onsport@gmail.com?subject=Message de ${name}&body=${message}`;
+
+              // Ouvre la fenêtre de composition de l'e-mail
+              window.location.href = mailToLink;
+
+              setName('');
+              setEmail('');
+              setMessage('');
+              // setEmailSent(true);
+            }}
+          >
+            Envoyer par email
+          </Button>
+          {/* {emailSent && (
+        <Message positive>
+          <Message.Header>Votre message a été envoyé avec succès</Message.Header>
+          <p>Merci de nous avoir contactés.</p>
+        </Message> */}
+          {/* )} */}
+        </Form>
+      </Grid>
+      <Footer />
+    </>
   );
 }
 
