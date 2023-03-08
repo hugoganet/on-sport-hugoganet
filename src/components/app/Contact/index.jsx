e5a312ba6f9e0dee75c9efc07ee58c85bb921bd0
Ramazan <ramazancinar0858@gmail.com>
2023-03-08 16:26:10 +0100

    Contact & About pages : CSS done (?)

--- src/components/app/Contact/index.jsx
+++ src/components/app/Contact/index.jsx
@@ -5,7 +5,8 @@ import './style.scss';
 
 import React, { useState } from 'react';
 import {
-  Form, Button, Message, Grid,
+  Form, Button, Message,
+  Grid,
 } from 'semantic-ui-react';
 import Header from '../Header';
 import Footer from '../Footer';
@@ -34,66 +35,46 @@ function Contact() {
     setName('');
     setEmail('');
     setMessage('');
-    // setEmailSent(true);
   };
 
   return (
     <>
       <Header />
       <h1 className="Contact__title">Nous contacter</h1>
-      <Grid className="Contact__div" centered>
-        <Form className="Contact__form" onSubmit={handleSubmit} error={formError}>
-          <Form.Input
-            label="Nom et prénom"
-            placeholder="Entrez votre nom et votre prénom"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <Form.Input
-            label="Email"
-            placeholder="Entrez votre email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <Form.TextArea
-            label="Message"
-            placeholder="Entrez votre message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
-          <Message
-            error
-            header="Erreur"
-            content="Veuillez remplir tous les champs."
-          />
-          <Button type="submit">Envoyer</Button>
-          <Button
-            onClick={() => {
-              // Compose l'e-mail avec les informations du formulaire
-              const mailToLink = `mailto:contact.onsport@gmail.com?subject=Message de ${name}&body=${message}`;
-
-              // Ouvre la fenêtre de composition de l'e-mail
-              window.location.href = mailToLink;
-
-              setName('');
-              setEmail('');
-              setMessage('');
-              // setEmailSent(true);
-            }}
-          >
-            Envoyer par email
-          </Button>
-          {/* {emailSent && (
-        <Message positive>
-          <Message.Header>Votre message a été envoyé avec succès</Message.Header>
-          <p>Merci de nous avoir contactés.</p>
-        </Message> */}
-          {/* )} */}
-        </Form>
+      <Grid stackable centered columns={1}>
+        <Grid.Column width={10}>
+          <Form fluid size="large" className="Contact__form" onSubmit={handleSubmit} error={formError}>
+            <Form.Input
+              width={40}
+              label="Nom et prénom"
+              placeholder="Entrez votre nom et votre prénom"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <Form.Input
+              label="Email"
+              placeholder="Entrez votre email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <Form.TextArea
+              label="Message"
+              placeholder="Entrez votre message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            />
+            <Message
+              error
+              header="Erreur"
+              content="Veuillez remplir tous les champs."
+            />
+            <Button primary type="submit">Envoyer</Button>
+          </Form>
+        </Grid.Column>
       </Grid>
       <Footer />
     </>
