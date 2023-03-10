4c319e5bdcec0894d654beb2d8f82b8e47227b22
Ramazan <ramazancinar0858@gmail.com>
2023-03-10 10:18:32 +0100

    Error page: add image & title

---
+++ src/components/app/Errors/index.js
@@ -0,0 +1,15 @@
+import './style.scss';
+
+import { Image } from 'semantic-ui-react';
+import errorImage from '../../../assets/error-image.png';
+
+function NotFound() {
+  return (
+    <div className="error__div">
+      <h1 className="error__title">Oups ! Il semblerait  que vous vous soyez perdu(e) dans notre site</h1>
+      <Image src={errorImage} />
+    </div>
+  );
+}
+
+export default NotFound;
