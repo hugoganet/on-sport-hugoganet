4f8ac3bac9ac1356db0b2f0d7ad4ea270254792d
Ramazan <ramazancinar0858@gmail.com>
2023-03-10 11:02:11 +0100

    Error page : add home button

--- src/components/app/Errors/index.jsx
+++ src/components/app/Errors/index.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/react-in-jsx-scope */
 import './style.scss';
 
-import { Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Image, Button } from 'semantic-ui-react';
 import errorImage from '../../../assets/error-image.png';
+// import Header from '../Header';
+// import Footer from '../Footer';
 
 function NotFound() {
   return (
     <div className="error__div">
       <h1 className="error__title">Oups ! Il semblerait  que vous vous soyez perdu(e) dans notre site</h1>
-      <Image src={errorImage} />
+      <Image centered src={errorImage} />
+      <Link to="/">
+        <Button primary size="big">
+          Retourner à l'accueil
+        </Button>
+      </Link>
     </div>
   );
 }
