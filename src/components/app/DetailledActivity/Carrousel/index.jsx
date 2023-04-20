86f72eb0e0ace464593684d23307d33913c48482
Hugo Ganet <ganethugo@gmail.com>
2023-04-20 14:13:19 +0200

    clean : get rid of carousel-react-rcdev dependencies

--- src/components/app/DetailledActivity/Carrousel/index.jsx
+++ src/components/app/DetailledActivity/Carrousel/index.jsx
@@ -47,54 +47,8 @@ function Carrousel({ photos }) {
       })()}
 
     </Swiper>
-
-  // <CarouselProvider
-  //   naturalSlideWidth={50}
-  //   naturalSlideHeight={50}
-  //   totalSlides={3}
-
-  // >
-  //   <Slider>
-  //     <Slide index={0}><Image src="/img/autre_bg.jpg" /></Slide>
-  //     <Slide index={0}><Image src="/img/fitness_bg.jpg" /></Slide>
-  //     <Slide index={0}><Image src="/img/glisse_montagne_bg.jpg" /></Slide>
-  //   </Slider>
-  //   <DotGroup />
-  //   <div className="carousel__buttons">
-  //     <ButtonBack>Back</ButtonBack>
-  //     <ButtonNext>Next</ButtonNext>
-  //   </div>
-  // </CarouselProvider>
   );
 }
-
-// // Core modules imports are same as usual
-// import { Navigation, Pagination } from 'swiper';
-// // Direct React component imports
-// import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
-
-// // Styles must use direct files imports
-// import 'swiper/swiper.scss'; // core Swiper
-// import 'swiper/modules/navigation/navigation.scss'; // Navigation module
-// import 'swiper/modules/pagination/pagination.scss'; // Pagination module
-
-// function Carousel() {
-//     return (
-//         <Swiper
-//         modules={[Navigation, Pagination ]}
-//         spaceBetween={50}
-//         slidesPerView={3}
-//         navigation
-//         pagination={{ clickable: true }}
-//         onSwiper={(swiper) => console.log(swiper)}
-//         onSlideChange={() => console.log('slide change')}
-//         >
-//             <SwiperSlide>Slide 1</SwiperSlide>
-//             <SwiperSlide>Slide 2</SwiperSlide>
-//             <SwiperSlide>Slide 3</SwiperSlide>
-//         </Swiper>
-//     );
-// }
 
 Carrousel.propTypes = {
   photos: PropTypes.arrayOf(
